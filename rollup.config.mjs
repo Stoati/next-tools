71919d3b8e3b4c05823b0cebcd77deaa0e6a52b8
Arzh <hugo@chataigner.com>
2023-12-17 20:28:01 +0100

    Try something

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,33 +2,28 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import preserveDirectives from "rollup-plugin-preserve-directives";
 
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
+    output: {
+      preserveModules: true,
+      dir: 'dist'
+
+    },
     plugins: [
+      preserveDirectives(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
+  // {
+  //   input: "dist/esm/types/index.d.ts",
+  //   output: [{ file: "dist/index.d.ts", format: "esm" }],
+  //   plugins: [dts()],
+  // },
 ];
